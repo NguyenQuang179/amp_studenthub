1ea6a4745e57267017ddd2373ab079adfe16e98e
NguyenQuang179 <nnquanglop96dt@gmail.com>
2024-03-28 22:41:04 +0700

    Add Bloc & Refactor Router

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,10 @@
 import 'package:amp_studenthub/configs/constant.dart';
-import 'package:amp_studenthub/screens/job_details_screen.dart';
-import 'package:amp_studenthub/screens/Message/message_detail.dart';
-import 'package:amp_studenthub/screens/Student_Projects/project_detail.dart';
-import 'package:amp_studenthub/screens/Student_Projects/project_list_filtered.dart';
-import 'package:amp_studenthub/screens/Student_Projects/project_list_saved.dart';
-import 'package:amp_studenthub/screens/navigation_menu.dart';
-import 'package:amp_studenthub/screens/post_job_screen.dart';
+import 'package:amp_studenthub/routes/app_router.dart';
 import 'package:flutter/material.dart';
-import 'package:go_router/go_router.dart';
 import 'package:google_fonts/google_fonts.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(const MyApp());
 }
 
 ThemeData _buildTheme(brightness) {
@@ -31,56 +24,17 @@ ThemeData _buildTheme(brightness) {
 }
 
 class MyApp extends StatelessWidget {
-  MyApp({super.key});
-  final GoRouter _router = GoRouter(routes: [
-    GoRoute(
-      path: '/',
-      builder: (context, state) => const NavigationMenu(),
-    ),
-    GoRoute(
-      path: '/postJob',
-      builder: (context, state) => const PostJobScreen(),
-    ),
-    GoRoute(
-      path: '/jobDetails',
-      builder: (context, state) => const JobDetailsScreen(),
-    ),
-    GoRoute(
-      path: '/projectDetail',
-      builder: (context, state) => const ProjectDetail(
-        jobTitle: 'Front-End Developer (React JS)',
-        jobCreatedDate: '16/03/2024',
-        jobDuration: '1-3 months',
-        jobStudentNeeded: 5,
-        jobProposalNums: 10,
-        jobExpectation: 'React JS, HTML, CSS, JavaScript',
-      ),
-    ),
-    GoRoute(
-      path: '/projectListSaved',
-      builder: (context, state) => const ProjectListSaved(),
-    ),
-    GoRoute(
-      path: '/projectListFiltered',
-      builder: (context, state) => const ProjectListFiltered(),
-    ),
-    GoRoute(
-      path: '/messageList',
-      builder: (context, state) => const ProjectListFiltered(),
-    ),
-    GoRoute(
-      path: '/messageDetail',
-      builder: (context, state) => const MessageDetail(),
-    ),
-  ]);
+  const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp.router(
-      routerConfig: _router,
       title: 'StudentHub',
       debugShowCheckedModeBanner: false,
       theme: _buildTheme(Brightness.light),
+      routeInformationProvider: AppRouter().router.routeInformationProvider,
+      routeInformationParser: AppRouter().router.routeInformationParser,
+      routerDelegate: AppRouter().router.routerDelegate,
     );
   }
 }
