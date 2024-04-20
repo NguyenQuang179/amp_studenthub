8922f021e41a084f8df890a3e1591407aee2584f
nien11102002 <nguyenduynien11102002@gmail.com>
2024-04-21 01:54:18 +0700

    Done assign API for Company Profile

--- lib/models/company_profile.dart
+++ lib/models/company_profile.dart
@@ -1,25 +1,21 @@
 class CompanyProfile {
-  final String id;
-  final String fullname;
+  final num id;
   final String companyName;
   final int size;
   final String website;
   final String description;
 
-  CompanyProfile(this.id, this.fullname, this.companyName, this.size,
-      this.website, this.description);
+  CompanyProfile(
+      this.id, this.companyName, this.size, this.website, this.description);
 
   CompanyProfile.fromJson(Map<String, dynamic> json)
-      : id = json['id'] as String,
-        fullname = json['name'] as String,
+      : id = json['id'] as int,
         companyName = json['companyName'] as String,
         size = json['size'] as int,
         website = json['website'] as String,
-        description = json['descriptions'] as String;
+        description = json['description'] as String;
 
   Map<String, dynamic> toJson() => {
-        'id': id,
-        'name': fullname,
         'companyName': companyName,
         'size': size,
         'website': website,
